39c5bcf78799104bbffb568e5d1c5cb7d651eb4b
Chaosjan44 <j.m.schniebs@gmail.com>
2024-07-20 23:02:57 +0200

    Update login to check for perm_login

--- login.php
+++ login.php
@@ -20,24 +20,28 @@ if (isset($_POST['action'])) {
                 $user = $stmt->fetch();
                 //Überprüfung des Passworts
                 if ($user !== false && password_verify($passwort, $user['password'])) {
-                    $_SESSION['userid'] = $user['user_id'];
-                    $identifier = md5(uniqid());
-                    $securitytoken = md5(uniqid());
-                    
-                    $stmt = $pdo->prepare("INSERT INTO securitytokens (user_id, identifier, securitytoken) VALUES (?, ?, ?)");
-                    $stmt->bindValue(1, $user['user_id'], PDO::PARAM_INT);
-                    $stmt->bindValue(2, $identifier);
-                    $stmt->bindValue(3, sha1($securitytoken));
-                    $result = $stmt->execute();
-                    if (!$result) {
-                        error_log("Error #2 while user login");
+                    if ($user['perm_login'] == 1) {
+                        $_SESSION['userid'] = $user['user_id'];
+                        $identifier = md5(uniqid());
+                        $securitytoken = md5(uniqid());
+                        
+                        $stmt = $pdo->prepare("INSERT INTO securitytokens (user_id, identifier, securitytoken) VALUES (?, ?, ?)");
+                        $stmt->bindValue(1, $user['user_id'], PDO::PARAM_INT);
+                        $stmt->bindValue(2, $identifier);
+                        $stmt->bindValue(3, sha1($securitytoken));
+                        $result = $stmt->execute();
+                        if (!$result) {
+                            error_log("Error #2 while user login");
+                            exit;
+                        }
+                        setcookie("identifier",$identifier,time()+(3600*24*365),"/"); //Valid for 1 year
+                        setcookie("securitytoken",$securitytoken,time()+(3600*24*365),"/"); //Valid for 1 year
+                        $error_msg = "<span class='text-success'>Anmeldung Erfolgreich!<br><br></span>";
+                        echo("<script>location.href='/internal/internal.php'</script>");
                         exit;
+                    } else {
+                        $error_msg = "<span class='text-danger'>Diese*r User ist nicht für die Anmeldung berechtigt!<br><br></span>";
                     }
-                    setcookie("identifier",$identifier,time()+(3600*24*365),"/"); //Valid for 1 year
-                    setcookie("securitytoken",$securitytoken,time()+(3600*24*365),"/"); //Valid for 1 year
-                    $error_msg = "<span class='text-success'>Anmeldung Erfolgreich!<br><br></span>";
-                    echo("<script>location.href='/overview.php'</script>");
-                    exit;
                 } else {
                     $error_msg = "<span class='text-danger'>Passwort passt nicht zum Anmeldename!<br><br></span>";
                 }
@@ -68,24 +72,24 @@ echo $buffer;
                         <?=$error_msg?>
                         <form action="login.php" method="post">
                             <div class="form-floating mb-3">
-                                <input id="inputUser" type="text" name="user" placeholder="User" autofocus class="form-control border-0 ps-4 text-dark" required>
-                                <label for="inputUser" class="text-dark">Anmeldename</label>
+                                <input id="inputUser" type="text" name="user" placeholder="User" autofocus class="form-control ps-4" required>
+                                <label for="inputUser">Anmeldename</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input id="inputPassword" type="password" name="passwort" placeholder="Passwort" class="form-control border-0 ps-4 text-dark" required>
-                                <label for="inputPassword" class="text-dark">Passwort</label>
+                                <input id="inputPassword" type="password" name="passwort" placeholder="Passwort" class="form-control seconda ps-4" required>
+                                <label for="inputPassword">Passwort</label>
                             </div>
                             <div class="<?php if (!isMobile()) {print('row row-cols-2 justify-content-between');} ?>">
                                 <div class="col">
                                     <div class="input-group <?php if (isMobile()) {print('mb-3 justify-content-center');} ?>">
                                         <label for="customCheck1" class="input-group-text">Angemeldet bleiben</label>
                                         <div class="input-group-text">
-                                            <input value="remember-me" id="customCheck1" type="checkbox" name="angemeldet_bleiben" value="1" class="form-check-input checkbox-kolping" checked>
+                                            <input value="remember-me" id="customCheck1" type="checkbox" name="angemeldet_bleiben" value="1" class="form-check-input checkbox-secondary" checked>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col <?php if (!isMobile()) {print('text-end');} else {print('text-center');} ?>">
-                                    <button type="submit" name="action" value="login" class="btn btn-kolping btn-floating">Anmelden</button>
+                                    <button type="submit" name="action" value="login" class="btn btn-secondary btn-floating ctext">Anmelden</button>
                                 </div>
                             </div>
                         </form>
